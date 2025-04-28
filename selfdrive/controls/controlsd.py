9119c66342b7a24e6e222dd816119162e77bfdd8
kaokao654 <140773554+kaokao654@users.noreply.github.com>
2025-04-28 21:38:18 +0800

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -351,8 +351,8 @@ class Controls:
           self.events.add(EventName.cameraMalfunction)
         elif not self.sm.all_freq_ok(self.camera_packets):
           self.events.add(EventName.cameraFrameRate)
-    if not REPLAY and self.rk.lagging:
-      self.events.add(EventName.controlsdLagging)
+    # if not REPLAY and self.rk.lagging:
+    #   self.events.add(EventName.controlsdLagging)
     if len(self.sm['radarState'].radarErrors) or (not self.rk.lagging and not self.sm.all_checks(['radarState'])):
       self.events.add(EventName.radarFault)
     if not self.sm.valid['pandaStates']:
